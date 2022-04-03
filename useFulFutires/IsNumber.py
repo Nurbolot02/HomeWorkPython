a8a3a7123eabbb274791433f7964ea8226f0c413
Nurbolot Gulamidinov <Gulamidinov02@mail.ru>
2022-04-04 02:56:18 +0600

    update usefulFutires - IsNumber

--- useFulFutires/IsNumber.py
+++ useFulFutires/IsNumber.py
@@ -4,3 +4,8 @@ def isNumber():
         number = input('Enter the number: ')
         if number.isdigit():
             return int(number)
+def isNumber(string):
+    while True:
+        number = input(string)
+        if number.isdigit():
+            return int(number)
