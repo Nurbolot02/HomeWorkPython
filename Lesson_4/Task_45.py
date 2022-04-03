0b3dc9f7d16b005077cb2150f2c7198353a64976
Nurbolot Gulamidinov <Gulamidinov02@mail.ru>
2022-04-04 02:10:36 +0600

    add Lesson_4 - Task_45 - Convert number to binary RU: конвертация из числа в бинарный код

---
+++ Lesson_4/Task_45.py
@@ -0,0 +1,8 @@
+# 45.	Написать программу преобразования десятичного числа в двоичное
+from useFulFutires.IsNumber import isNumber
+from useFulFutires.ToBinary import toBinary
+
+print('45.	Написать программу преобразования десятичного числа в двоичное')
+number = isNumber()
+result = toBinary(number)
+print(result)
