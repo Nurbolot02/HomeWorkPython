c5559b9d224d7e55e1fe0e57a4bdf3c82735856e
Nurbolot <ngulamidinov@gmail.com>
2022-04-13 14:33:05 +0600

    add Test/FunctionInFunction.py

---
+++ Test/FunctionInFunction.py
@@ -0,0 +1,21 @@
+from useFulFutires.IsNumber import isNumber
+
+def exponentiation(x):
+    return x ** 2
+
+def math(function, x):
+    print(function(x))
+
+x = isNumber()
+math(exponentiation, x)
+
+
+
+
+
+
+
+
+
+
+
