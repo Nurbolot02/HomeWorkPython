d8cd6995b3bf35068cf4daf7e2cbcd8ed82cb540
Nurbolot <ngulamidinov@gmail.com>
2022-04-28 15:39:23 +0600

    update : Test working with files

--- Test/working_with_files.py
+++ Test/working_with_files.py
@@ -1,15 +1,27 @@
-colors = ['Grean', 'Blue', '\nYellow']
-data = open('text.txt', 'w')
-for i in colors:
-    data.write(i)
-    data.write(' ')
-data.close()
+colors = {'Grean','Blue','Yellow'}
 path = 'text.txt'
-
-with open(path,'r') as data:
-    for i in data:
-        print(i)
+count = 1
+with open(path, 'r+') as file:
+    for i in colors:
+        file.write(f'{count} ')
+        count += 1
+        file.write(i)
+        file.write('\n')
+    s = file.read()
+    print(s)
+# data = open('text.txt', 'w')
+# for i in colors:
+#     data.write(colors[i])
+#     data.write(' ')
+# data.close()
+# path = 'text.txt'
 #
+# with open(path,'') as data:
+#     for i in data:
+#         data.write(colors[i])
+#         data.write(' ')
+#         print(i)
+# #
 # def new_string(symbol, count = 3):
 #     return symbol * count
 # print(new_string(5))
@@ -41,26 +53,26 @@ with open(path,'r') as data:
 # print(dictionary)
 # for i in dictionary:
 #     print(i,' ', dictionary[i])
-print('*-------------------------------------------------------------------------*')
-numbers_1 = {1,2,3,4,5,6}
-numbers_2 = {5,8,46,46,6}
-
-numbersUnion = numbers_1.union(numbers_2)
-print(numbers_1)
-print(numbers_2)
-
-numbersSection = numbers_1.intersection(numbers_2)
-numOneDiffNumTwo = numbers_1.difference(numbers_2)
-numTwoDiffOne = numbers_2.difference(numbers_1)
-
-print(numbersSection)
-print(numOneDiffNumTwo)
-print(numTwoDiffOne)
-
-numbersUnionFrozenSet = frozenset(numbersUnion)
-
-numbers_11 = [1,2,3,4,5,6]
-print(numbers_11)
-numbers_11.pop(2)
-numbers_11.insert(0, 5)
-print(numbers_11)
+# print('*-------------------------------------------------------------------------*')
+# numbers_1 = {1,2,3,4,5,6}
+# numbers_2 = {5,8,46,46,6}
+#
+# numbersUnion = numbers_1.union(numbers_2)
+# print(numbers_1)
+# print(numbers_2)
+#
+# numbersSection = numbers_1.intersection(numbers_2)
+# numOneDiffNumTwo = numbers_1.difference(numbers_2)
+# numTwoDiffOne = numbers_2.difference(numbers_1)
+#
+# print(numbersSection)
+# print(numOneDiffNumTwo)
+# print(numTwoDiffOne)
+#
+# numbersUnionFrozenSet = frozenset(numbersUnion)
+#
+# numbers_11 = [1,2,3,4,5,6]
+# print(numbers_11)
+# numbers_11.pop(2)
+# numbers_11.insert(0, 5)
+# print(numbers_11)
