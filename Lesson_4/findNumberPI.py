d8b223738942cec6e43b9fe1d119919e62aff7e0
Nurbolot <ngulamidinov@gmail.com>
2022-05-11 23:03:21 +0600

    add Lesson_4  findNumberPI # Вычислить число Пи c заданной точностью d

---
+++ Lesson_4/findNumberPI.py
@@ -0,0 +1,22 @@
+# Вычислить число Пи c заданной точностью d
+# Пример:
+#
+# при $d = 0.001, π = 3.141.$ $10^{-1} ≤ d ≤10^{-10}$
+
+
+# Импорт библиотеки math
+import math
+
+# Функция
+def Accuracy(data):
+    i = 0
+    while(1 > data):
+        i += 1
+        data = data * 10
+    return i
+
+def AccuracyPI(precision):
+    return round(math.pi, precision)
+
+precision = float(input('Input precision (example: 0.001): '))
+print(f'{AccuracyPI(Accuracy(precision))}')
