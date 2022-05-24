c48b4df01bdae3674aee24840656a2e2699c22ea
Nurbolot <ngulamidinov@gmail.com>
2022-05-24 13:35:23 +0600

    update Lesson_8  : Employees RU: # Обновлен код # добавлено удаление контактов

--- Lesson_8/controller.py
+++ Lesson_8/controller.py
@@ -1,19 +1,22 @@
 from Lesson_8.out.menu import get_menu
-from Lesson_8.input import user_answer
-from Lesson_8.prog.employee import add_employee, show_employee_list, generate_random_employee, delete_employee, load_employees
+from Lesson_8.input import user_answer, init_data
+from Lesson_8.prog import add_employee, show_employee_list, generate_random_employee, delete_employee
+from Lesson_8.prog.find_employee import find_employee
 def start():
     while True:
-        load_employees(False)
+        employee_list = init_data.init()
         get_menu()
         user_select = user_answer.read_user_answer()
         if user_select == 0:
             print('GoodBye! See you later!')
             break
         elif user_select == 1:
-            add_employee()
+            add_employee.add_employee(employee_list)
         elif user_select == 2:
-            show_employee_list()
+            show_employee_list.show_employee_list()
         elif user_select == 3:
-            generate_random_employee()
+            generate_random_employee.generate_random_employee(employee_list)
         elif user_select == 4:
-            delete_employee()
+            find_employee(employee_list)
+        elif user_select == 5:
+            delete_employee.delete_employee(employee_list)
---
+++ Lesson_8/prog/generate_random_employee.py
@@ -0,0 +1,17 @@
+from Lesson_8.input.user_answer import read_user_answer
+from Lesson_8.prog.generate_random_phone_number import generate_random_phone_number
+from Lesson_8.prog.generate_random_string import generate_random_string
+from Lesson_8.prog.end_program import end_program
+from Lesson_8.prog.save_employee import save_employee
+
+def generate_random_employee(employee_list):
+    f = True
+    NumberText = '\n1 - Добавить еще рандомного сотрудника \n0 - Выйти в главное меню \nВыберите операцию: '
+    while f:
+        read_user_answer_text = 'Сколько рандомных сотрудников вы хотите добавить?: '
+        count = read_user_answer(read_user_answer_text, maxNumber=1000000)
+        for i in range(count):
+            employee_list.append({'Name': generate_random_string(8), 'SurName': generate_random_string(8), 'Position': generate_random_string(8), 'Salary': generate_random_phone_number()})
+        f = end_program(NumberText)
+    save_employee(employee_list)
+
