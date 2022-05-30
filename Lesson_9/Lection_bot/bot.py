fc284e647f8e0b48bc39ed9f06c022e86d7b8a73
Nurbolot <ngulamidinov@gmail.com>
2022-05-30 15:32:01 +0600

    update Lesson_9  : Lection_Bot add ExchangeRate RU: # Добавлено функция для работы с курсом валют

--- Lesson_9/Lection_bot/bot.py
+++ Lesson_9/Lection_bot/bot.py
@@ -1,7 +1,7 @@
 from telegram.ext import Updater, CommandHandler
 from bot_commands import *
 
-updater = Updater("5384058738:AAEf4Nqx6ox56GIHVzz1g8KtJEo7GcDU-Yg")
+updater = Updater("5384058738:AAEUp6Xw78o5uDYxC6vzaJnKlDMPxt8KgQs")
 
 updater.dispatcher.add_handler(CommandHandler("hello", hello))
 updater.dispatcher.add_handler(CommandHandler("hi", hi))
@@ -10,6 +10,9 @@ updater.dispatcher.add_handler(CommandHandler("help", help))
 updater.dispatcher.add_handler(CommandHandler("start", help))
 updater.dispatcher.add_handler(CommandHandler("sum", sum))
 updater.dispatcher.add_handler(CommandHandler("math", math))
+updater.dispatcher.add_handler(CommandHandler("Exchange", Exchange))
+updater.dispatcher.add_handler(CommandHandler("Calc_Exchange", Calc_Exchange))
+updater.dispatcher.add_handler(CommandHandler("getVideo", getVideo))
 
 print('Start Bot')
 updater.start_polling()
--- Lesson_9/Lection_bot/bot_commands.py
+++ Lesson_9/Lection_bot/bot_commands.py
@@ -3,6 +3,68 @@ from telegram.ext import CallbackContext
 from useFulFutires.MatN import do
 from log import log
 import datetime
+import requests
+EXtext = ''
+startEX = 0
+
+def getVideo(update: Update, context: CallbackContext):
+    path = "C:\\Users\\Gulam\\Videos\\GB\\2022_01_20_GeekbrainsGendir_2_L1-Часть 3.mp4"
+    update.message.reply_video(path)
+
+def get_value(f = False, RUB = 1):
+    data = requests.get('https://www.cbr-xml-daily.ru/daily_json.js').json()
+    date_vlue = data['Valute']
+    EXtext = ''
+    Curse = ['KGS', 'KZT', 'USD', 'EUR', 'TRY', 'CNY']
+
+    if RUB > 0:
+        for i in date_vlue.items():
+            # print(i[1]['Name'], i[1]['Nominal'] / i[1]['Value'])
+            # print(float('{0:.3f}'.format(i[1]['Nominal'] / i[1]['Value'])))
+            if i[0] == Curse[0] or i[0] == Curse[1] or i[0] == Curse[2] or i[0] == Curse[3] or i[0] == Curse[4] or i[
+                0] == Curse[5]:
+                EXtext += f"{RUB} - RUB == {float('{0:.3f}'.format(RUB * (i[1]['Nominal'] / i[1]['Value'])))} - {i[0]} \n"
+    return EXtext
+
+def getNumber(msg):
+    number = msg.split(" ")
+    if len(number) == 1:
+        return -1
+    for i in number:
+        if i.isdigit():
+            return int(i)
+    return -1
+
+def Calc_Exchange(update: Update, context: CallbackContext) -> None:
+    log(update, context)
+    number = getNumber(update.message.text)
+    text = ''
+    print(text)
+    if number != -1:
+        result = get_value(RUB=number)
+    else:
+        update.message.reply_text(f'Что то пошло не так '
+                                  f'\nПопробуйте ввести /Calc_Exchange и через пробел количество рублей! иначе ответа от меня не дождетесь ха-ха-ха '
+                                  f'\nПример: '
+                                  f'\n/Calc_Exchange 500')
+
+    update.message.reply_text(result)
+
+
+
+
+def Exchange(update: Update, context: CallbackContext) -> None:
+    log(update, context)
+    msg = update.message.text
+    text = ''
+    print(text)
+    try:
+        result = get_value()
+    except:
+        update.message.reply_text(f'Что то пошло не так попробуйте ввести все числа и символы через пробел!')
+
+    update.message.reply_text(result)
+
 
 
 def hello(update: Update, context: CallbackContext) -> None:
@@ -17,7 +79,15 @@ def hi(update: Update, context: CallbackContext) -> None:
 
 def help(update: Update, context: CallbackContext) -> None:
     log(update, context)
-    update.message.reply_text(f'/hi - Приветсвие \n/sum a b \n/math Введите все числа и символы через пробел иначе ответа от меня не дождетесь ха-ха \nПример: /math 2 + 2 + 4 - 5 * 5 \n/time - время \n/help - список команд')
+    update.message.reply_text(f'/hi - Приветсвие '
+                              f'\n/sum a b '
+                              f'\n/math Введите математическое выражение все числа и символы через пробел иначе ответа от меня не дождетесь ха-ха '
+                              f'\nПример: /math 2 + 2 + 4 - 5 * 5 '
+                              f'\n/time - время '
+                              f'\n/help - список команд'
+                              f'\n/Exchange - Возврашает курс валют'
+                              f'\n/Calc_Exchange (количество рублей) - Введите количество рублей которые хотите конвертировать через пробел иначе ответа от меня не дождетесь ха-ха-ха'
+                              ) #f'\n/getVideo - Возврашает видео')
 
 
 def time(update: Update, context: CallbackContext) -> None:
