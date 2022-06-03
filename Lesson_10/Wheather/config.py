61f2333024a90d05b05ea3d4dfd86662bcf9e6f9
Nurbolot <ngulamidinov@gmail.com>
2022-06-03 14:50:41 +0600

    add Lesson_10  : Weather_bot add config.py RU: # Добавлено чтение токена из файла

---
+++ Lesson_10/Wheather/config.py
@@ -0,0 +1,7 @@
+path = 'C:/tk/open_weather_token'
+with open(path, 'r') as file:
+    open_weather_key = file.read()
+
+path = "C:/tk/tg_token"
+with open(path, 'r') as file:
+    tg_bot_token = file.read()
